cdf85243f20e46aeb94d8d6d16bd0cb5be2c568b
ssp <2015suraj.pawar@ves.ac.in>
2019-01-08 20:22:04 +0530

    start bfs

---
+++ Pratice-programs/c++/graph/bfs.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void bfs(int [][], int, int);
+int main(){
+    int adj[3][3] = {
+                        {1,0,0},
+                        {0,1,0},
+                        {1,1,0},
+                    };
+    return 0;
+}
+void bfs(){
+
+}
