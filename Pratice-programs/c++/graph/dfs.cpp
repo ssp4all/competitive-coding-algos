b341afb78338ca8b3a23a35142da9a0715851ac8
ssp <2015suraj.pawar@ves.ac.in>
2019-01-09 15:44:55 +0530

    Add dfs

---
+++ Pratice-programs/c++/graph/dfs.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+#define NODE 10
+void dfs(int [][NODE], int [], int);
+
+int main(){
+    //int adj[NODE][NODE];
+    int adj [NODE][NODE] = {
+                        {0,1,0,1,0},
+                        {1,0,1,1,0},
+                        {0,1,0,0,1},
+                        {1,1,0,0,1},
+                        {0,0,1,1,0}
+                    };
+    int visited[NODE], i;
+    for(i=0; i<NODE; i++)
+        visited[i] = 0;
+    dfs(adj, visited, 0);
+    return 0;
+}
+void dfs(int adj[NODE][NODE], int visited[NODE], int start){
+    int i;
+    int top = -1;
+    int stack[NODE];
+
+    stack[++top] = start;
+    while(top != -1){
+
+        start = stack[top];
+
+        for(i=0; i<NODE; i++){
+            if(adj[start][i] == 1 && visited[i] == 0){
+                cout<<(char)(i+65)<<"\t";
+                stack[++top] = i;
+                visited[i] = 1;
+                break;
+            }
+        }
+        if(i == NODE)
+            top--;
+    }
+}
+
+
