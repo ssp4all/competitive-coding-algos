4b6c0959df2d03985f813426e8e9a9201be37266
ssp <2015suraj.pawar@ves.ac.in>
2019-02-06 19:12:42 +0530

    Update

--- codechef/FEB19B/HMAPPY.cpp
+++ codechef/FEB19B/HMAPPY.cpp
@@ -1,5 +1,4 @@
 // Suraj Pawar- ssp4all
-
 #include <bits/stdc++.h>
 # define lld long long int
 int main() {
@@ -10,19 +9,32 @@ int main() {
 		lld pa = 0;
 		lld pb = 0;
 		scanf("%lld %lld %lld %lld",&n,&a,&b,&k);
-		for(i=1; i<(n+1); i++){
+		for(i=0; i<(n+1); i=i+a){
 			if(i%a == 0 && i%b == 0){
 			}
-			else if(i%a == 0 && i%b != 0)
+			else if(i%a == 0 && i%b != 0){
 				pa++;
-			else if(i%a != 0 && i%b == 0)
-				pb++;
-			if((pa+pb)>=k){
+			}
+			if((pa)>=k){
 				printf("Win\n");
 				break;
+				// exit(0);
+			}
+		}
+		if(pa<k){
+			for(i=0; i<(n+1); i=i+b){
+				if(i%a == 0 && i%b == 0){
+				}
+				else if(i%a != 0 && i%b == 0){
+					pb++;
+				}
+				if((pa+pb)>=k){
+					printf("Win\n");
+					break;
+				}
 			}
 		}
 		if((pa+pb)<k)
 			printf("Lose\n");
 	}
-}
+}ou
--- hakerrank/python/maximize-it.py
+++ hakerrank/python/maximize-it.py
@@ -1,3 +1,5 @@
+
+# Suraj Pawar
 from itertools import product
 
 K, M = map(int, input().split())
@@ -5,20 +7,3 @@ N = (list(map(int, input().split()))[1:] for _ in range(K))
 results = map(lambda x: sum(i**2 for i in x) % M, product(*N))
 print(max(results))
 
-# Suraj Pawar
-k, m = map(int, input().split())
-ip = []
-op = []
-for _ in range(k):
-    n, *i = map(int, input().split())
-    ip.append(list(i))
-total = 0
-for j in ip:
-    maxi = 0
-    for k in j:
-        if maxi < (k**2) % m:
-            maxi = k**2
-    op.append(maxi)
-for i in op:
-    total += i
-print(int(total % m))
