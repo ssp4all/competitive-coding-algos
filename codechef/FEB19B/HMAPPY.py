d6e71461baa5f57f9089511c8e866519e77919e7
ssp <2015suraj.pawar@ves.ac.in>
2019-02-12 19:07:13 +0530

    Done HMPAYY2

--- codechef/FEB19B/HMAPPY.py
+++ codechef/FEB19B/HMAPPY.py
@@ -1,17 +1,18 @@
-# cook your dish here
-# Suraj Pawar- ssp4all
-t = int(input())
-for _ in range(t):
+# Suraj Pawar
+from fractions  import gcd
+def LCM(a, b):
+    return (int)(a*b)/gcd(a,b)
+    
+for _ in range(int(input())):
 	n, a, b, k = map(int, input().split())
-	pa, pb = 0, 0
-	for i in range(1,n+1):
-		if i%a == 0 and i%b == 0:
-			continue
-		elif i%a == 0 and i%b != 0:
-			pa += 1
-		elif i%b == 0 and i%a != 0:
-			pb += 1
-	if (pa+pb) >= k:
+	pa = (int)(n/a)
+	pb = (int)(n/b)
+	lcm = LCM(a ,b)
+	lcm = (int)(n/lcm)
+# 	print(lcm)
+	pa -= lcm 
+	pb -= lcm
+	if (pa+pb)>= k:
 		print('Win')
 	else:
 		print('Lose')
