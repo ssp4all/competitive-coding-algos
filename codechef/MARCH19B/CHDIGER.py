a91c393f6f27fcb56d4a34a23caf256e6cbaec11
ssp <2015suraj.pawar@ves.ac.in>
2019-03-10 18:44:26 +0530

    Add CC

--- codechef/MARCH19B/CHDIGER.py
+++ codechef/MARCH19B/CHDIGER.py
@@ -2,18 +2,18 @@
 # https://www.codechef.com/MARCH19B/problems/CHDIGER
 for _ in range(int(input())):
 	n, d = map(int, input().split())
-	# op = 0
-	# for c in str(n):
-	# 	if int(c)>=d:
-	# 		op += 1
-	# print(op)
-	r = max([int(c) for c in str(n)])
-	if r<d:
-		print(n)
-	else:
-		# n = int(n)	
-		while d<r:
-			r = max([int(c) for c in str(n)])
-			n = int(n)*10 + d
-			n = str(n).replace(str(r), "", 1)
-		print(int(n))
+	ip = list(map(int, str(n)))
+	count = len(ip)
+	t = 0
+	ans = []
+	for i in ip:
+		if i>=d:	t += 1
+		else:
+			while ans != [] and ans[-1]>i:
+				ans.pop()
+				t += 1
+			ans.append(i) 
+	temp = [d]*(count-len(ans))
+	ans.extend(temp)
+	ans = int("".join(map(str, ans)))
+	print(ans)
