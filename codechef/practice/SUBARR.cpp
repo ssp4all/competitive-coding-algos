ceca0fd218d8e8959b83136fb0a315eb900f1ec3
ssp <2015suraj.pawar@ves.ac.in>
2019-03-17 21:11:35 +0530

    Add

--- codechef/practice/SUBARR.cpp
+++ codechef/practice/SUBARR.cpp
@@ -72,3 +72,101 @@ int main(){
     cout<<ans<<endl;
     return 0;
 }
+
+//merge sort approach
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <map>
+using namespace std;
+
+#define vit vector<int>
+#define vst vector<string>
+#define loop(i,n) for(int i = 0;i < n; i++)
+#define loopi(i,a,n) for(int i = a;i <= n; i++)
+#define pb push_back
+#define mp make_pair
+typedef long long ll;
+
+ll ans;
+ll a[4000005];
+ll c[2000001];
+int s;
+void sort(ll a[], ll low, ll high, ll mid) 
+{
+    ++s;
+    ll i = low;
+    ll j = mid + 1;
+    ll k = low;
+    for (; i <= mid && j <= high;) {
+        if (a[i] <= a[j]) {
+            c[k] = a[i];
+            ans += high - j + 1;
+            cout<<"ans "<<ans<<endl;
+            k++;
+            i++;
+        }
+        else {
+            c[k] = a[j];
+            k++;
+            j++;
+        }
+    }
+    for (; i <= mid;) {
+        c[k] = a[i];
+        k++;
+        i++;
+    }
+    for (; j <= high;) {
+        c[k] = a[j];
+        k++;
+        j++;
+    }
+    for (i = low; i <= high; i++)
+        a[i] = c[i];
+}
+
+void merge(ll a[], ll low, ll high) {
+
+    ll mid;
+    if (low < high) {
+        mid = (low + high) / 2;
+        cout<<low<<" "<<high<<" "<<mid<<endl;
+        merge(a, low, mid);
+        cout<<'r'<<endl;
+        cout<<mid+1<<" "<<high<<endl;
+        merge(a, mid + 1, high);
+        cout<<'d'<<endl;
+
+        sort(a, low, high, mid);
+    }
+
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    
+    ll n, k;
+    cin >> n >> k;
+    ans = 0;
+    ll i;
+    for (i = 1; i <= n; i++)
+    {
+        cin>>a[i];
+        a[i] -= k;
+        a[i] += a[i - 1];
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    merge(a, 0, n);
+    for (i = 0; i < n; i++)
+        cout<<a[i]<<" ";
+
+    cout<<endl;
+    cout << ans<<" "<<s;
+    return 0;
+}
