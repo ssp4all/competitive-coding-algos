6b9ed0d0eff9f9394ad8a07d748dfa467d1c7524
ssp <2015suraj.pawar@ves.ac.in>
2019-03-30 22:06:26 +0530

    Update structure

--- codechef/FEB19B/MAGICJAR.py
+++
@@ -1,8 +0,0 @@
-# Suraj Pawar 
-# https://www.codechef.com/FEB19B/problems/MAGICJAR
-for _ in range(int(input())):
-	n = int(input())
-	ip = list(map(int, input().split()))
-	for i in range(n):
-		ip[i] -= 1
-	print(sum(ip)+1) 
