84bc6d524b597d637783c96d14de185a21ca836c
Suraj <suraj.pawar1898@gmail.com>
2021-02-12 22:07:04 -0500

    aah..more code

--- algorithms/sorting_algos.py
+++ algorithms/sorting_algos.py
@@ -26,7 +26,6 @@ class Solution:
             q = (p+r) // 2
             self.merge_sort(p, q)
             self.merge_sort(q+1, r)
-            print("p q r", p, q, r)
             self.merge(p, q, r)
 
 ################################################
