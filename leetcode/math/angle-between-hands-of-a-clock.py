028827a73149bee4d1cb51ef95d496dab8c9bf39
Suraj Pawar <ssp4all@users.noreply.github.com>
2023-06-29 20:44:36 -0400

    add more comments

--- leetcode/math/angle-between-hands-of-a-clock.py
+++ leetcode/math/angle-between-hands-of-a-clock.py
@@ -7,8 +7,11 @@ class Solution:
         MINUTE_ANGLE = 6 
 
         min_angle = MINUTE_ANGLE * minutes 
-        # we'll convert hour:minutes to hours only by adding minutes and then dividing by 60 bcoa 1hr = 60 min
-        hr_angle = HOUR_ANGLE * (hour % 12 + minutes / 60)
-        # print(min_angle, hr_angle)
+        # we'll convert hour:minutes to hours only by 
+        # adding minutes and then dividing by 60 bcoa 1hr = 60 min
+        hr_angle = HOUR_ANGLE * (hour % 12) 
+        # The hour hand of the clock moves 360 degree in 
+        # 12 hours(12x60 min) which means (360/(12x60)) =0.5 degree in a minute
+        hr_angle += minutes * 0.5
         diff = abs(min_angle - hr_angle)
         return min(diff, 360 - diff) # 01:57
